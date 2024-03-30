8ec4e6c4d588730f06add00c6dd8fe9b94976631
Mark Whitaker <markw@bitrot.net>
2024-03-30 09:55:58 +0000

    Improve tests

--- MonthDiff.Tests/DateTimeOffsetTests.cs
+++ MonthDiff.Tests/DateTimeOffsetTests.cs
@@ -5,66 +5,132 @@ public class DateTimeOffsetTests
     [Test]
     public void TestOneMonthApart()
     {
-        Assert.That(new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2014, 2, 1, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(1));
+        var from = new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2014, 2, 1, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestJustUnderOneMonthApart()
     {
-        Assert.That(new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2014, 1, 31, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(0));
+        var from = new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2014, 1, 31, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 0;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestJustOverOneMonthApart()
     {
-        Assert.That(new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2014, 2, 2, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(1));
+        var from = new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2014, 2, 2, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void Test31JanuaryTo28February()
     {
-        Assert.That(new DateTimeOffset(2014, 1, 31, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2014, 2, 28, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(1));
+        var from = new DateTimeOffset(2014, 1, 31, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2014, 2, 28, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestLeapYear29FebruaryTo29March()
     {
-        Assert.That(new DateTimeOffset(2012, 2, 29, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2012, 3, 29, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(1));
+        var from = new DateTimeOffset(2012, 2, 29, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2012, 3, 29, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestWholeYearMinusOneDay()
     {
-        Assert.That(new DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2012, 12, 31, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(11));
+        var from = new DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2012, 12, 31, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 11;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestWholeYear()
     {
-        Assert.That(new DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2013, 1, 1, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(12));
+        var from = new DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2013, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 12;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestLeapYear28FebruaryToNonLeapYear28February()
     {
-        Assert.That(new DateTimeOffset(2012, 2, 29, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2013, 2, 28, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(12));
+        var from = new DateTimeOffset(2012, 2, 29, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2013, 2, 28, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 12;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void Test100Years()
     {
-        Assert.That(new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2100, 1, 1, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(1200));
+        var from = new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2100, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 1200;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestSameDate()
     {
-        Assert.That(new DateTimeOffset(2014, 8, 5, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2014, 8, 5, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(0));
+        var from = new DateTimeOffset(2014, 8, 5, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2014, 8, 5, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 0;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestPastDate()
     {
-        Assert.That(new DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero).GetTotalMonthsFrom(new DateTimeOffset(2011, 6, 10, 0, 0, 0, TimeSpan.Zero)), Is.EqualTo(6));
+        var from = new DateTimeOffset(2012, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var to = new DateTimeOffset(2011, 6, 10, 0, 0, 0, TimeSpan.Zero);
+        const int expectedDiffInMonths = 6;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 }
--- MonthDiff.Tests/DateTimeTests.cs
+++ MonthDiff.Tests/DateTimeTests.cs
@@ -6,66 +6,132 @@ public class DateTimeTests
     [Test]
     public void TestOneMonthApart()
     {
-        Assert.That(new DateTime(2014, 1, 1).GetTotalMonthsFrom(new DateTime(2014, 2, 1)), Is.EqualTo(1));
+        var from = new DateTime(2014, 1, 1);
+        var to = new DateTime(2014, 2, 1);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestJustUnderOneMonthApart()
     {
-        Assert.That(new DateTime(2014, 1, 1).GetTotalMonthsFrom(new DateTime(2014, 1, 31)), Is.EqualTo(0));
+        var from = new DateTime(2014, 1, 1);
+        var to = new DateTime(2014, 1, 31);
+        const int expectedDiffInMonths = 0;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestJustOverOneMonthApart()
     {
-        Assert.That(new DateTime(2014, 1, 1).GetTotalMonthsFrom(new DateTime(2014, 2, 2)), Is.EqualTo(1));
+        var from = new DateTime(2014, 1, 1);
+        var to = new DateTime(2014, 2, 2);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void Test31JanuaryTo28February()
     {
-        Assert.That(new DateTime(2014, 1, 31).GetTotalMonthsFrom(new DateTime(2014, 2, 28)), Is.EqualTo(1));
+        var from = new DateTime(2014, 1, 31);
+        var to = new DateTime(2014, 2, 28);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestLeapYear29FebruaryTo29March()
     {
-        Assert.That(new DateTime(2012, 2, 29).GetTotalMonthsFrom(new DateTime(2012, 3, 29)), Is.EqualTo(1));
+        var from = new DateTime(2012, 2, 29);
+        var to = new DateTime(2012, 3, 29);
+        const int expectedDiffInMonths = 1;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestWholeYearMinusOneDay()
     {
-        Assert.That(new DateTime(2012, 1, 1).GetTotalMonthsFrom(new DateTime(2012, 12, 31)), Is.EqualTo(11));
+        var from = new DateTime(2012, 1, 1);
+        var to = new DateTime(2012, 12, 31);
+        const int expectedDiffInMonths = 11;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestWholeYear()
     {
-        Assert.That(new DateTime(2012, 1, 1).GetTotalMonthsFrom(new DateTime(2013, 1, 1)), Is.EqualTo(12));
+        var from = new DateTime(2012, 1, 1);
+        var to = new DateTime(2013, 1, 1);
+        const int expectedDiffInMonths = 12;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestLeapYear28FebruaryToNonLeapYear28February()
     {
-        Assert.That(new DateTime(2012, 2, 29).GetTotalMonthsFrom(new DateTime(2013, 2, 28)), Is.EqualTo(12));
+        var from = new DateTime(2012, 2, 29);
+        var to = new DateTime(2013, 2, 28);
+        const int expectedDiffInMonths = 12;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void Test100Years()
     {
-        Assert.That(new DateTime(2000, 1, 1).GetTotalMonthsFrom(new DateTime(2100, 1, 1)), Is.EqualTo(1200));
+        var from = new DateTime(2000, 1, 1);
+        var to = new DateTime(2100, 1, 1);
+        const int expectedDiffInMonths = 1200;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestSameDate()
     {
-        Assert.That(new DateTime(2014, 8, 5).GetTotalMonthsFrom(new DateTime(2014, 8, 5)), Is.EqualTo(0));
+        var from = new DateTime(2014, 8, 5);
+        var to = new DateTime(2014, 8, 5);
+        const int expectedDiffInMonths = 0;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 
     [Test]
     public void TestPastDate()
     {
-        Assert.That(new DateTime(2012, 1, 1).GetTotalMonthsFrom(new DateTime(2011, 6, 10)), Is.EqualTo(6));
+        var from = new DateTime(2012, 1, 1);
+        var to = new DateTime(2011, 6, 10);
+        const int expectedDiffInMonths = 6;
+
+        var actualDiffInMonths = from.GetTotalMonthsFrom(to);
+
+        Assert.That(actualDiffInMonths, Is.EqualTo(expectedDiffInMonths));
     }
 }
